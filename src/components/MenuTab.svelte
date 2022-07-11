6efa3e658d608d9603a9128bc9a4bca05b2c4432
nutthanon <nutthanon.tho@gmail.com>
2022-07-11 16:18:58 +0700

    [+] add: menuTab Component

---
+++ src/components/MenuTab.svelte
@@ -0,0 +1,40 @@
+<div class="menu-tab">
+  <div class="menu">
+    <p class="menu-item">Contact</p>
+    <div class="menu-content" />
+  </div>
+  <div class="menu">
+    <p class="menu-item">Discord</p>
+    <div class="menu-content" />
+  </div>
+</div>
+
+<style>
+  .menu-tab {
+    display: flex;
+    margin: auto 1rem;
+    gap: 2rem;
+  }
+  .menu-item {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  .menu-item::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background-color: black;
+    border-radius: 0.1rem;
+    transform: scaleX(0);
+    transition: transform 0.3s ease;
+  }
+
+  .menu-item:hover::after {
+    transform: scaleX(1);
+  }
+</style>
